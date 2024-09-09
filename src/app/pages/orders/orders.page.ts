c28bee34060b881363377da57f56990c19cba79f
shreya <shreya4um@gmail.com>
2024-09-09 23:59:10 +0530

    order page

--- src/app/pages/orders/orders.page.ts
+++ src/app/pages/orders/orders.page.ts
@@ -6,8 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.page.scss'],
 })
 export class OrdersPage implements OnInit {
+  public progress = 0;
+  constructor() { 
 
-  constructor() { }
+
+    setInterval(() => {
+      this.progress += 0.01;
+
+      // Reset the progress bar when it reaches 100%
+      // to continuously show the demo
+      if (this.progress > 1) {
+        setTimeout(() => {
+          this.progress = 0;
+        }, 1000);
+      }
+    }, 50);
+  }
 
   ngOnInit() {
   }
